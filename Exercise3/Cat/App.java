6dbc8ca2b1a870435f5df63c39bef52eaa5b9a46
Joshua Guimbaolibot <119920263+jumbo-hotdog@users.noreply.github.com>
2023-05-16 22:21:01 +0800

    Create App.java

---
+++ Exercise3/Cat/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Pet myPet = new Pet();
+
+        myPet.purr();
+        System.out.println(myPet.eyeColor);
+        System.out.println(myPet.hairColor);
+    }
+}
