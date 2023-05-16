227976f1d595f521bf1d48598545627dd0305e8f
Joshua Guimbaolibot <119920263+jumbo-hotdog@users.noreply.github.com>
2023-05-16 22:32:39 +0800

    Create App.java

---
+++ Exercise3/Dog/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Create object from pet class
+        Labradoodle myPet = new Labradoodle();
+
+        myPet.sleep();
+        System.out.println(myPet.odor);
+        System.out.println(myPet.height);
+    }
+}
