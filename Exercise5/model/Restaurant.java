4a1ea9ad7dbf12055891d206cfbc7d10168f1d72
Joshua Guimbaolibot <119920263+jumbo-hotdog@users.noreply.github.com>
2023-05-23 12:27:08 +0800

    Add files via upload

---
+++ Exercise5/model/Restaurant.java
@@ -0,0 +1,23 @@
+package model;
+
+class Restaurant {
+
+    private String product;
+    private String taste;
+
+    public String getProduct() {
+        return this.product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getTaste() {
+        return this.taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+}
